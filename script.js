49d92f9ec61cb897f69e8a5d957d4c539956c27c
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2025-08-30 12:38:49 +0300

    Refactor addItem function for better code organization and clarity

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
-  $("#addBtn").click(function () {
+  function addItem() {
     let value = $("#itemInput").val().trim();
     if (value === "") {
       $("#errorMsg").fadeIn().delay(2000).fadeOut();
     } else {
       $("#itemList").append(`<li>${value} <button class="deleteBtn">Delete</button></li>`);
       $("#itemInput").val("");
+    }
+  }
+
+  $("#addBtn").click(function () {
+    addItem();
+  });
+
+  $("#itemInput").keypress(function (e) {
+    if (e.which === 13) { // Enter key
+      addItem();
     }
   });
 
